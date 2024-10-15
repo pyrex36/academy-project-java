7f52c1d8d329643e9a8ce129b826d9e92532039d
pyrex <72110245+pyrex36@users.noreply.github.com>
2024-10-15 09:26:34 -0400

    new_folder

---
+++ src/main/java/com/bptn/academy/maven_project_java/App.java
@@ -0,0 +1,7 @@
+package com.bptn.academy.maven_project_java;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/com/bptn/academy/maven_project_java/AppTest.java
@@ -0,0 +1,13 @@
+package com.bptn.academy.maven_project_java;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
